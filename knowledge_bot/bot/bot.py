786044f2100cd116101271f9efaccf1987eb3d22
Miguel Osuna <miguelosuna@pop-os.localdomain>
2020-07-17 20:46:25 -0700

    Add base files

---
+++ knowledge_bot/bot/bot.py
@@ -0,0 +1,8 @@
+# Standard library imports
+
+# Third party library imports
+
+# Local library imports 
+
+if __name__ == "__main__":
+    pass
---
+++ knowledge_bot/translator/translator.py
@@ -0,0 +1,8 @@
+# Standard library imports
+
+# Third party imports
+
+# Local application imports 
+
+if __name__ == "__main__":
+    pass
